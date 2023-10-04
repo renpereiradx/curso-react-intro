63beb819b275e5da9c4097976c08c07ce2b61d62
Rene Pereira <renedx92@gmail.com>
2023-10-03 22:27:06 -0300

    Implementando loading skeleton

--- src/components/App/AppUI.js
+++ src/components/App/AppUI.js
@@ -3,6 +3,9 @@ import { TodoSearch } from '../TodoSearch';
 import { TodoList } from '../TodoList';
 import { TodoItem } from '../TodoItem';
 import { CreateTodoButton } from '../CreateTodoButton';
+import { TodosLoading } from '../TodosLoading';
+import { TodosError } from '../TodosError';
+import { EmptyTodos } from '../EmptyTodos';
 
 function AppUI({
   loading,
@@ -20,9 +23,15 @@ function AppUI({
       <TodoCounter totalTodos={totalTodos} completedTodos={completedTodos} />
       <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
       <TodoList>
-        {loading && <p>Cargando</p>}
-        {error && <p>Hubo un error!</p>}
-        {!loading && searchedTodos.length === 0 && <p>Todo vacio</p>}
+        {loading && (
+          <>
+            <TodosLoading />
+            <TodosLoading />
+            <TodosLoading />
+          </>
+        )}
+        {error && <TodosError />}
+        {!loading && searchedTodos.length === 0 && <EmptyTodos />}
         {searchedTodos.map((todo) => {
           return (
             <TodoItem
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import { AppUI } from './AppUI';
 import { useLocalStorage } from './useLocalStorage';  
 
-
 /* const defaultTodos = [
   { text: 'Cortar cebolla', completed: true },
   { text: 'Tomar el curso de intro a React', completed: false },
